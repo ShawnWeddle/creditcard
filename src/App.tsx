f8484edc97c6e1630595bff0811564d4f26ff622
unknown <marlinzeah@gmail.com>
2024-12-19 16:36:00 -0500

    Design change

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import DialogMain from "./components/dialog/Dialog";
 
 function App() {
   return (
-    <div className="bg-gray-500 min-h-screen flex justify-center">
+    <div className="bg-gradient-to-br from-red-100 via-amber-200 to-lime-100  min-h-screen flex justify-center">
       <div className="w-full py-14 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg">
         <DialogMain />
         <Main />
--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,25 +1,10 @@
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import CreditCard from "../card/CreditCard";
 
 const Main: React.FC = () => {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Enter your Credit Card information</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <div className="flex justify-center">
-          <CreditCard />
-        </div>
-      </CardContent>
-      <CardFooter>Thanks</CardFooter>
-    </Card>
+    <div className="flex justify-center">
+      <CreditCard />
+    </div>
   );
 };
 
